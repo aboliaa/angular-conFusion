67d41b2a963fea90d6558eee499c39f48b677637
Aboli Aradhye <aboli.a.aradhye@gmail.com>
2017-09-09 22:57:12 +0530

    Configure Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +13,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+	BrowserModule,
+	BrowserAnimationsModule,
+	MaterialModule,
+	FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
