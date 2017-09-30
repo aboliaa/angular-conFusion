72d9b5bba000e760831293afd583c25b08e71320
Aboli Aradhye <aboli.a.aradhye@gmail.com>
2017-09-30 14:28:18 +0530

    reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import 'hammerjs';
 
@@ -42,7 +43,8 @@ import { LoginComponent } from './login/login.component';
 	MaterialModule,
 	FlexLayoutModule,
 	AppRoutingModule,
-	FormsModule
+	FormsModule,
+	ReactiveFormsModule
   ],
   providers: [
 			DishService,
